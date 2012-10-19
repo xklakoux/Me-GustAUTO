aa8e19ea56bfe1bea822188013e5068e1d374682
xklakoux <artur@(none)>
2012-10-19 20:56:16 +0200

    added model of database to jpa

---
+++ src/es/uc3m/ctw/me_gustauto/model/Price.java
@@ -0,0 +1,62 @@
+package es.uc3m.ctw.me_gustauto.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+
+/**
+ * The persistent class for the prices database table.
+ * 
+ */
+@Entity
+@Table(name="prices")
+public class Price implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+
+	@Lob
+	private String descr;
+
+	private String name;
+
+	private BigDecimal price;
+
+	public Price() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescr() {
+		return this.descr;
+	}
+
+	public void setDescr(String descr) {
+		this.descr = descr;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+}
