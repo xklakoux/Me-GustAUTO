be21787af414a3bb389abe1ac5dece16dbebe140
xklakoux <artur@(none)>
2012-10-28 11:28:59 +0100

    database records

--- mgauto_db.sql
+++ mgauto_db.sql
@@ -64,7 +64,16 @@ CREATE TABLE prices(
 
 -- users
 insert into users (username, full_name, hash, salt, email, phone, address, role) values('admin', 'Admin Adminov', '733b8fa4b833ca9d3baec9470d7f467614e85057', 'NLVBGCODGE', 'asdsa@dasdsa.dasda' ,'123131313', 'dasdada', 'admin');
-
 insert into users (username, full_name, hash, salt, email, phone, address, role) values('xklakoux', 'Artur Staniec', 'e8e8921b196428ac5ef1f05265d360f2078910af', 'INAEBQWCKQ', 'adsad@dasdsa.ds', '123131313', 'dasdada', 'admin');
 
+insert into general_ads (title, descr, user_id) values("Garage for sale", "lot of space, super nice", 1);
+insert into general_ads (title, descr, user_id) values("Viagra", "will get you up and running in seconds", 0);
+insert into general_ads (title, descr, user_id) values("Rooms for students", "without furniture or anything, ideal for students!", 1);
+insert into general_ads (title, descr, user_id) values("Lollipops", "sweet and stuff, for kids and everyone", 1);
+insert into general_ads (title, descr, user_id) values("Computers", "used, new, for your grandma and grandpa too", 1);
+insert into general_ads (title, descr, user_id) values("Cars", "other super site for cars, forget about this", 1);
+
+insert into prices (name, price, descr) values ("1 month", 20, "normal price");
+insert into prices (name, price, descr) values ("2 month", 16, "at least 2 months 10/month discount!");
+insert into prices (name, price, descr) values ("3 month", 8, "at least 3 months ultra discount!");
 
